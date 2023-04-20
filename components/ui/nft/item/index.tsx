1bfc5aa09db93bd79ee29ecea569c350fc699ce6
alframirez <alframirezg@gmail.com>
2023-04-20 06:53:31 -0400

    changes nft item card

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -23,7 +23,7 @@ const NftItem: FunctionComponent<NftItemProps> = ({ item, buyNft }) => {
             alt="New NFT"
           />
         </div>
-        <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+        <div className="flex-1 bg-white p-6 flex flex-col justify-between" >
           <div className="flex-1">
             <div className="flex justify-between items-center">
               <div className="flex items-center mt-2">
@@ -35,8 +35,8 @@ const NftItem: FunctionComponent<NftItemProps> = ({ item, buyNft }) => {
                   />
                 </div>
                 <div className="ml-3">
-                  <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">Creator</p>
-                  <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">{shortifyAddress(item.creator)}</p>
+                  <p className="text-sm font-medium text-indigo-600">Creator</p>
+                  <p className="text-xs font-medium text-black">{shortifyAddress(item.creator)}</p>
                 </div>
               </div>
               <p className="text-sm font-medium text-indigo-600">
@@ -44,31 +44,23 @@ const NftItem: FunctionComponent<NftItemProps> = ({ item, buyNft }) => {
               </p>
             </div>
             <div className="block mt-2">
-              <p className="text-xl font-semibold text-gray-900">{item.meta.name}</p>
-              <p className="mt-3 mb-3 text-base text-gray-500">{item.meta.description}</p>
+              <p className="text-xl font-semibold text-black">{item.meta.name}</p>
+              <p className="mt-3 mb-3 text-base text-black">{item.meta.description}</p>
             </div>
           </div>
           <div className="overflow-hidden mb-4">
             <dl className="-mx-4 -mt-4 flex flex-wrap">
               <div className="flex flex-col px-4 pt-4">
-                <dt className="order-2 text-sm font-medium text-gray-500">Price</dt>
+                <dt className="order-2 text-sm font-medium ">Price</dt>
                 <dd className="order-1 text-xl font-extrabold text-indigo-600">
                   <div className="flex justify-center items-center">
                     {item.price}
-                    <img className="h-6" src="/images/small-eth.webp" alt="ether icon" />
+                    {/* <img className="h-6" src="/images/small-eth.webp" alt="ether icon" /> */}
+                    <img className="h-6" src="/images/bnb-logo.webp" alt="bnb icon" />
                   </div>
                 </dd>
               </div>
-              {item.meta.attributes.map(attribute =>
-                <div key={attribute.trait_type} className="flex flex-col px-4 pt-4">
-                  <dt className="order-2 text-sm font-medium text-gray-500">
-                    {attribute.trait_type}
-                  </dt>
-                  <dd className="order-1 text-xl font-extrabold text-indigo-600">
-                    {attribute.value}
-                  </dd>
-                </div>
-              )}
+
             </dl>
           </div>
           <div>
