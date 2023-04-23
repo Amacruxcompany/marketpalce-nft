e99d8c75e1ce90db6bd622fc28f6d82ee55de0ed
alframirez <alframirezg@gmail.com>
2023-04-23 18:22:31 -0400

    10mb string

--- pages/nft/createPass.tsx
+++ pages/nft/createPass.tsx
@@ -399,7 +399,7 @@ const NftCreate: NextPage = () => {
                               <p className='pl-1'>or drag and drop</p>
                             </div>
                             <p className='text-xs text-gray-500'>
-                              PNG, JPG, GIF up to 1MB
+                              PNG, JPG, GIF up to 10MB
                             </p>
                           </div>
                         </div>
